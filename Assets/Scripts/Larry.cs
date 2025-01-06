8aa67d47431a1897172836053d4dc275a4736910
Korvek <e.gattermann@li-ga.de>
2025-01-06 17:31:51 +0100

    Beispielszene Aufbau

--- Assets/Scripts/Larry.cs
+++ Assets/Scripts/Larry.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
 
 
@@ -22,39 +24,46 @@ public class Larry : MonoBehaviour
 
     //InputActions um auf Tastendrücke zu reagieren
     public InputAction drehenAktion;
-    public InputAction sammelnAktion;
+    public InputAction sammelAktion;
 
     //Richtungsvektor. Gibt die Bewegungsrichtung an
     private Vector2 bewegung;
     //Körperkomponente
     private Rigidbody2D rigidbody2d;
+    //Sammelbares Objekt, das berührt wird
+    private GameObject sammelObjekt;
     
     private void Awake()
     {
         //Verknüpfe drehenAktion mit der RichtungsWechsel Methode
         drehenAktion.performed += RichtungWechsel;
+        //Verknüpfe sammelAktion mit der Sammeln Methode
+        sammelAktion.performed += Sammeln;
         //Hole Rigidbody Komponente des Objekts
         rigidbody2d = GetComponent<Rigidbody2D>();
         //Initialisierung des Richtungsvektors
         bewegung = new Vector2(0f, 0f);
     }
 
+    
+
     private void OnEnable()
     {
         //Aktiviere InputActions
         drehenAktion.Enable();
-        sammelnAktion.Enable();
+        sammelAktion.Enable();
     }
     private void OnDisable()
     {
         //Deaktiviere InputActions
         drehenAktion.Disable();
-        sammelnAktion.Disable();
+        sammelAktion.Disable();
     }
     private void OnDestroy()
     {
         //Löse Verknüpfung
         drehenAktion.performed -= RichtungWechsel;
+        sammelAktion.performed -= Sammeln;
     }
 
     void FixedUpdate()
@@ -93,7 +102,7 @@ public class Larry : MonoBehaviour
     /// <summary>
     /// Wechselt die aktuelle Bewegungsrichtung im Uhrzeigersinn
     /// </summary>
-    void RichtungWechsel(InputAction.CallbackContext obj)
+    void RichtungWechsel(InputAction.CallbackContext context)
     {
         //Wenn nicht der letzte Eintrag im Richtungsenum erreicht ist, wechsle einen Eintrag weiter
         if ( ((int)richtung) != 3)
@@ -123,6 +132,32 @@ public class Larry : MonoBehaviour
                 break;
             default:
                 break;
+        }
+    }
+    /// <summary>
+    /// Sammelt das Momentan berührte Objekt ein
+    /// </summary>
+    private void Sammeln(InputAction.CallbackContext context)
+    {
+        if (sammelObjekt != null)
+        {
+            //TODO Objekt entfernen, Buchstaben prüfen, Wort anzeigen
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="Sammelbar")
+        {
+            sammelObjekt = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Sammelbar")
+        {
+            sammelObjekt = null;
         }
     }
 }
