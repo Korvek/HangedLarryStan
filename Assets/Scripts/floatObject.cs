d141c71861955cc0f85722af5d2a0595ac3484b6
Korvek <e.gattermann@li-ga.de>
2025-01-08 13:25:15 +0100

    Timer

---
+++ Assets/Scripts/floatObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="ScriptableObjects/Zahl")]
+public class floatObject : ScriptableObject
+{
+    public float value;
+}
