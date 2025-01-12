c0db0533c532fc650a8649c2c4ec753bab356462
Korvek <e.gattermann@li-ga.de>
2025-01-12 17:01:14 +0100

    Ausgang wechselt Level

--- Assets/Scripts/Animationen/Rechtskurve.cs
+++ Assets/Scripts/Animationen/Rechtskurve.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
-public class Rechtskurve : StateMachineBehaviour
+public class RechtsKurve : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
