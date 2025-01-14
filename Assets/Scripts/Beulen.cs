52849f06fb38817cc722683829257e8adc610c99
Korvek <e.gattermann@li-ga.de>
2025-01-14 15:36:06 +0100

    Cleanup + Kommentare: Beulen, Routen

--- Assets/Scripts/Beulen.cs
+++ Assets/Scripts/Beulen.cs
@@ -6,28 +6,47 @@ using UnityEngine.UI;
 
 public class Beulen : MonoBehaviour
 {
+    /// <summary>
+    /// Maximale Anzahl an erlaubten Beulen
+    /// </summary>
     public int maxBeulen;
-    public int beulen;
+    /// <summary>
+    /// Levelreset Event
+    /// </summary>
     public GameEvent resetGame;
+    /// <summary>
+    /// Bilder für steigende Anzahl Beulen
+    /// </summary>
     public List<Sprite> beulenSprites;
+    /// <summary>
+    /// Farbe für steigende Anzahl Beulen
+    /// </summary>
     public List<Color> beulenColors;
 
+    // Aktuelle Anzahl Beulen
+    private int beulen;
+    // Image Komponente
     private Image img;
     private void Awake()
     {
+        //Hole Image Komponente
         img = GetComponent<Image>();
     }
+    /// <summary>
+    /// Kollisionsfunktion. Erhöht Beulen Anzahl und startet Level neu, wenn nötig
+    /// </summary>
     public void Autsch()
     {
-        beulen++;
+        beulen++; //Erhöhe Beulen Anzahl
+        //Wenn die maximale Beulenzahl überschritten ist
         if (beulen > maxBeulen)
         {
-            resetGame.TriggerEvent();
+            resetGame.TriggerEvent(); //Starte Level neu
         }
         else
         {
-            img.overrideSprite = beulenSprites[beulen];
-            img.color = beulenColors[beulen];
+            img.overrideSprite = beulenSprites[beulen]; //Lade nächstes Bild aus Liste
+            img.color = beulenColors[beulen]; //Lade nächste Farbe aus Liste
         }
     }
 }
--- Assets/Scripts/Routen.cs
+++ Assets/Scripts/Routen.cs
@@ -4,28 +4,12 @@ using UnityEngine;
 
 public class Routen : MonoBehaviour
 {
-    public GameEvent resetGame;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.LogWarning("Enter"+collision.gameObject.name);
-    }
+    /// <summary>
+    /// Kollisionsevent
+    /// </summary>
+    public GameEvent kollisionEvent;
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.LogWarning("Exit"+collision.gameObject.name);
-        resetGame.TriggerEvent();
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
+        kollisionEvent.TriggerEvent();
     }
 }
