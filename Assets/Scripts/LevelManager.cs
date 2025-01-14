66ea63b0ad5fd3ea36bac6b4c3c41e703fac3b8c
Korvek <e.gattermann@li-ga.de>
2025-01-14 15:46:19 +0100

    Skripte bewegt und Kommentare

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,15 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    /// <summary>
+    /// Lade das nächste Level
+    /// </summary>
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.loadedSceneCount+1);
     }
+    /// <summary>
+    /// Beende das Spiel
+    /// </summary>
     public void EndGame()
     {
         UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
+    /// <summary>
+    /// Starte das aktuelle Level neu
+    /// </summary>
     public void ResetGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
