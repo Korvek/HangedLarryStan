65264d77e5f8a4cf3ffadbe54b68666b1085ba2b
Korvek <e.gattermann@li-ga.de>
2025-01-14 12:54:40 +0100

    Doppelsammlung Buchstaben behoben

--- Assets/Scripts/Animationen/Rechtskurve.cs
+++ Assets/Scripts/Animationen/Rechtskurve.cs
@@ -21,27 +21,27 @@ public class RechtsKurve : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Vector3 position = animator.gameObject.transform.position;
-        
+
         //90° Drehung
         animator.gameObject.transform.Rotate(0f, 0f, -90f);
-        Debug.Log("Position vor Bewegung: "+ position);
+        Debug.Log("Position vor Bewegung: " + position);
         //Halbe Länge addieren
         position = position +
             (animator.gameObject.transform.up *
             (animator.gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2f));
-        Debug.Log("Position vor Rundung: "+position);
-        Debug.Log("Bewegung: "+(animator.gameObject.transform.up *
+        Debug.Log("Position vor Rundung: " + position);
+        Debug.Log("Bewegung: " + (animator.gameObject.transform.up *
             (animator.gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2f)));
         //Position Runden
-        if(animator.gameObject.transform.up == Vector3.up || animator.gameObject.transform.up == Vector3.down)
+        if (animator.gameObject.transform.up == Vector3.up || animator.gameObject.transform.up == Vector3.down)
         {
-            position.x = (Mathf.Round(2f*position.x)/2f);
+            position.x = (Mathf.Round(2f * position.x) / 2f);
         }
         else if (animator.gameObject.transform.up == Vector3.right || animator.gameObject.transform.up == Vector3.left)
         {
-            position.y = (Mathf.Round(2f*position.y)/2f);
+            position.y = (Mathf.Round(2f * position.y) / 2f);
         }
-        Debug.Log("Position nach Rundung: "+position);
+        Debug.Log("Position nach Rundung: " + position);
         animator.gameObject.transform.position = position;
         animator.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
     }
