0a656a3d2a0e71365a58d5b956726158724f4ae8
Korvek <e.gattermann@li-ga.de>
2025-01-16 09:42:05 +0100

    Mehr VolumeSlider

--- Assets/Scripts/Volume.cs
+++ Assets/Scripts/Volume.cs
@@ -6,7 +6,15 @@ using UnityEngine.Audio;
 public class Volume : MonoBehaviour
 {
     public AudioMixer mixer;
-    public void SetLevel(float sliderValue)
+    public void SetLevelMaster(float sliderValue)
+    {
+        mixer.SetFloat("MusikVolume", Mathf.Log10(sliderValue) * 20);
+    }
+    public void SetLevelMusik(float sliderValue)
+    {
+        mixer.SetFloat("MusikVolume", Mathf.Log10(sliderValue) * 20);
+    }
+    public void SetLevelEffekte(float sliderValue)
     {
         mixer.SetFloat("MusikVolume", Mathf.Log10(sliderValue) * 20);
     }
