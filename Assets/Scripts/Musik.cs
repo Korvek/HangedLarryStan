7953c1e3bcb22743dfd2c02d0202935918a80abb
Korvek <e.gattermann@li-ga.de>
2025-01-22 20:26:00 +0100

    Kleine Änderungen

--- Assets/Scripts/Musik.cs
+++ Assets/Scripts/Musik.cs
@@ -6,6 +6,12 @@ public class Musik : MonoBehaviour
 {
     private void Awake()
     {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Musik");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
         DontDestroyOnLoad(gameObject);
     }
 }
